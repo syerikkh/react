115fb3aa95f4474567dbb32fb4eea05d603ff938
94241021 Khuanysh <23LP2243@G304-CC0080.local>
2023-12-06 19:34:40 +0800

    12.06

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,47 +6,48 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-      <header>
-        <nav>
-          <div className='nav-left'>
+      <header class="h-[68px] w-[1440px] flex justify-center py-[16px] px-[80px]">
+        <nav class="flex w-[100%] justify-between px-[32px]">
+          <div >
             <img src="/logo.jpg" alt='' />
           </div>
-          <div className='nav-right'>
+          <div class="flex gap-[24px] items-center">
             <p1>About</p1>
             <p1>Work</p1>
             <p1>Testimonials</p1>
             <p1>Contact</p1>
-            <img className='icon' src="/Icon.png" alt='' />
-            <div className='downloadBtn'>Download CV</div>
+            <img class="h-6 w-6" src="/Icon.png" alt='' />
+            <div class="bg-black w-[136px] text-white py-[6px] px-[16px] rounded-xl flex justify-center items-center">Download CV</div>
           </div>
         </nav>
       </header>
 
-      <main>
-        <div className='container'>
-          <div className='bio-container'>
-            <h1 className='bio-header'>Hi, I’m Sagar 👋</h1>
-            <p1 className='bio-paragraph'>I'm a full stack developer (React.js & Node.js) with a focus on creating (and occasionally designing) <br /> exceptional digital experiences that are fast, accessible, visually appealing, and responsive. Even<br /> though I have been creating web applications for over 7 years, I still love it as if it was something new.</p1>
-            <div className='bio-group'>
-              <div className='location'>
+      <main class="w-[1440px] py-16 px-20">
+        <div class="flex gap-12 px-8 justify-between">
+          <div class="flex flex-col gap-2">
+            <h1 class="text-6xl font-bold">Hi, I’m Sagar 👋</h1>
+            <p1 class="text-base font-normal">I'm a full stack developer (React.js & Node.js) with a focus on creating (and occasionally designing) <br /> exceptional digital experiences that are fast, accessible, visually appealing, and responsive. Even<br /> though I have been creating web applications for over 7 years, I still love it as if it was something new.</p1>
+            <div class="flex flex-col gap-2 mt-12">
+              <div class="flex gap-2">
                 <img src='location.png' />
                 Ahmedabad, India
               </div>
-              <div className='available'>
-                <div className='dot-icon-container'><img className='dot-icon' src='icon-dot.png' /></div>
+              <div class="flex gap-2">
+                <div class="h-6 w-6 p-2">
+                  <img src='icon-dot.png' /></div>
                 Available for new projects
               </div>
             </div>
-            <div className='bio-footer'>
-              <div className='contact'>
+            <div class="pt-12 ">
+              <div class="flex">
                 <img src='Icon Button 1.png' />
                 <img src='Icon Button 2.png' />
                 <img src='Icon Button 3.png' />
               </div>
             </div>
           </div>
-          <div className='picture-container'>
-            <img className="person-pic" src='Pic.png' />
+          <div class="h-90 w-100">
+            <img class="shadow-[40px_40px_#E5E7EB]" src='Pic.png' />
           </div>
         </div>
       </main>
