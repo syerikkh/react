cb6949b28752214b689319d44a4d45f25361a88b
94241021 Khuanysh <23LP2243@G304-CC0080.local>
2023-12-19 19:24:14 +0800

    12.19

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -1,32 +1,42 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Stopwatch = () => {
-    const [count, setCount] = useState(0);
-    const [toggle, setToggle] = useState(false);
+    const [time, setTime] = useState(0);
+    const [isRunning, setisRunning] = useState(false);
 
-    const myInterval = () => {
-        setInterval(() => {
-
-        }, 100);
-    }
-
-    const add = () => {
-        setCount(count + 1);
-    }
     useEffect(() => {
-        if (toggle) {
-            add()
+        let interval;
+        if (isRunning) {
+            interval = setInterval(() => { setTime(time + 1) }, 10);
         }
-    }, [toggle, count]);
+        return () => clearInterval(interval);
 
 
+    }, [isRunning, time]);
+
+    const hours = Math.floor(time / 360000);
+    const minutes = Math.floor((time % 360000) / 6000);
+    const seconds = Math.floor((time % 6000) / 100);
+    const mseconds = Math.floor(time % 100);
+
+    const startAndStop = () => {
+        setisRunning(!isRunning);
+    }
+    const reset = () => {
+        setTime(0);
+    }
     return (
-        <div class="flex justify-center mt-4 items-center gap-2">
-            {count}
-            <button onClick={() => setToggle(!toggle)} class="bg-black p-2 rounded-xl text-white">
-                {toggle ? "Stop" : "Start"}
-            </button>
-        </div>
+        <div class="flex justify-center items-center flex-col gap-2 bg-yellow-400 w-[25%] m-auto rounded-xl p-2">
+            <div class="text-black p-1 rounded-xl">Stopwatch</div>
+            <div class="flex gap-2 text-center justify-center items-center flex-col">
+                <p>{hours} : {minutes.toString().padStart(2, "0")}: {seconds.toString().padStart(2, "0")} : {mseconds.toString().padStart(2, "0")}</p>
+                <div class="flex gap-2">
+                    <button onClick={startAndStop} class="bg-black text-white p-1 rounded-lg">
+                        {isRunning ? "Stop" : "Start"}
+                    </button>
+                    <button onClick={reset} class="bg-black text-white p-1 rounded-lg">Reset</button>
+                </div>
+            </div>
+        </div >
     )
 }
