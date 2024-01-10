5d70f369894dab97b79914e40de8c104f4b35ad3
94241021 Khuanysh <23LP2243@G304-CC0080.local>
2024-01-10 19:29:44 +0800

    01.10

---
+++ src/context/ThemeCont.jsx
@@ -0,0 +1,13 @@
+// import { createContext, useState } from "react";
+
+// export const ThemeContext = createContext();
+
+// export const ThemeProvider = ({ children }) => {
+//     const [darkTheme, setDarkTheme] = useState(false);
+//     const toggleTheme = () => {
+//         setDarkTheme(!darkTheme);
+//     }
+//     <ThemeContext.Provider value={{ darkTheme, toggleTheme }}>
+//         {children}
+//     </ThemeContext.Provider>
+// }
